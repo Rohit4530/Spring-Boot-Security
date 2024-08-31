f2606118e3b5dafa2ae8f742fa609f4e18dd666e
Rohit4530 <rohitgunjal547@gmail.com>
2024-08-31 22:31:18 +0530

    added controller

---
+++ secure/src/main/java/com/spring/secure/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.spring.secure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to my application";
+    }
+}
